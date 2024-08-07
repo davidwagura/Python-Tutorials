de2965f3b3dbcbb1d897544a3c5fe9e2b4fda587
David wanjohi <davidwagura83@gmail.com>
2024-08-07 07:40:37 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+def display_menu():
+    print("Text Formatter Options:")
+    print("1. Capitalize All Words")
+    print("2. Convert to Uppercase")
+    print("3. Convert to Lowercase")
+    print("4. Count Words and Characters")
+    print("5. Find and Replace Words")
+    print("6. Reverse Text")
+    print("7. Decorative ASCII Art")
+    print("8. Exit")
+
+
+shop = "My newly opened shop"
+
+money = 20000
+
+print(len(shop))
+
+print(shop.upper())
+
+print(shop.lower())
+
+print(str(money))
+
+print("It costed us" + " " + str(200) + " " + "dollars")
